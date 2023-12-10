1f6dc89860e3e32dd8707b86033d2ec950682dea
m-skowronski <mskowronski547@gmail.com>
2023-12-10 14:57:19 +0100

    RPM-3 install: angular material, ngrx store, devtools

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,11 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideStore } from '@ngrx/store';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), provideStore(), provideAnimations(), provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() })]
 };
